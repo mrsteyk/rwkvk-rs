89afcf406586280787dbf20b1405a79ec601d0f1
Alexandr "MrSteyk" German <mrsteyk1@vk.com>
2023-02-13 14:29:34 +0500

    0.2.0

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -50,12 +50,27 @@ impl Rwkv {
         Ok(self.inner.rwkv().forward_raw(&x, &mut state.inner))
     }
 
-    pub fn forward(&self, tokens: &PyList, state: &mut State) -> PyResult<Vec<f32>> {
+    pub fn forward_raw_batch(&self, tokens: &PyList, state: &mut State) -> PyResult<Vec<f32>> {
         let tokens: Vec<Vec<f32>> = tokens.extract()?;
         for token in tokens.iter().take(tokens.len()-1) {
             self.inner.rwkv().forward_raw_preproc(token, &mut state.inner);
         }
         Ok(self.inner.rwkv().forward_raw(tokens.last().unwrap(), &mut state.inner))
+    }
+
+    pub fn forward_token(&self, token: usize, state: &mut State) -> PyResult<Vec<f32>> {
+        let x = self.inner.rwkv().emb.get(token).unwrap();
+        Ok(self.inner.rwkv().forward_raw(x, &mut state.inner))
+    }
+
+    pub fn forward(&self, tokens: &PyList, state: &mut State) -> PyResult<Vec<f32>> {
+        let tokens: Vec<usize> = tokens.extract()?;
+        for &token in tokens.iter().take(tokens.len()-1) {
+            let token = self.inner.rwkv().emb.get(token).unwrap();
+            self.inner.rwkv().forward_raw_preproc(token, &mut state.inner);
+        }
+        let token = self.inner.rwkv().emb.get(*tokens.last().unwrap()).unwrap();
+        Ok(self.inner.rwkv().forward_raw(token, &mut state.inner))
     }
 }
 
